a6a990f081073b8e788286f8aeff553b86c0a8e0
KALU ARACHCHIGE RUSIRU <seerusiru@gmail.com>
2021-08-13 22:12:44 +0900

    #bmi calcualtor app

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var HeightTextField: UITextField!
+    
+    @IBOutlet weak var WeightTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        var classification:String
+        var weight:Double = 0
+        var height:Double = 0
+        if let weightText = WeightTextField.text{
+            if let weightDouble = Double(weightText){
+                weight = weightDouble
+            }
+        }
+        
+        if let heightText = HeightTextField.text{
+            if let heightDouble = Double(heightText){
+                height = heightDouble
+            }
+        }
+        
+        let bmi = weight/(height * height)
+        
+        if bmi < 18.5{
+            classification = "Underweight"
+        }
+        else if bmi < 24.9 {
+            classification = "Healthy Weight"
+        }
+        else if bmi < 29.9 {
+            classification = "Overweight"
+        }
+        else{
+            classification = "Obese"
+        }
+        
+        let formattedBMI = String(format:"%.2f",bmi)
+        resultLabel.text = "BMI : \(formattedBMI),\(classification)"
+    
+        
+    }
+    
 }
 
